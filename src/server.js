708d280f090bdbb5ad6104bc8dcd0b06ee838c1c
marchisDev <buiquangchicv0303@gmail.com>
2025-01-16 11:31:37 +0700

    Done close connection mongodb

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 
 import express from 'express'
-import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB } from '~/config/mongodb'
+import AsyncExitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 
 const START_SERVER = () => {
   const app = express()
@@ -13,6 +13,7 @@ const START_SERVER = () => {
   app.get('/', async (req, res) => {
     console.log(await GET_DB().listCollections().toArray())
     res.end('<h1>Hello World!</h1><hr>')
+    process.exit(0)
   })
 
   app.listen(port, hostname, () => {
@@ -21,10 +22,13 @@ const START_SERVER = () => {
     )
   })
 
-  // exitHook(() => {
-  //   console.log('Exit App')
-  // })
+  AsyncExitHook(() => {
+    console.log('4. Closing MongoDB connection...')
+    CLOSE_DB()
+    console.log('5. MongoDB connection closed successfully!')
+  })
 }
+
 
 // chi khi ket noi thanh cong toi MongoDB thi moi chay server
 // Immediately Invoked Function Expression (IIFE) / Anonymous Async Function
