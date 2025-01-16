685d1f8a1dfd3d2098746230ab58c652e3e0c24b
marchisDev <buiquangchicv0303@gmail.com>
2025-01-16 20:39:38 +0700

    Config express router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,3 +1,15 @@
-/**
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
 
- */
+const Router = express.Router()
+
+// Test APIs V1 status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are working' })
+})
+
+// Board APIs
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
