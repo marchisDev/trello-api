3f78bde148aba96dc8c088a1086698cf960ebebd
marchisDev <buiquangchicv0303@gmail.com>
2025-01-17 18:13:36 +0700

    create board api

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -10,12 +11,16 @@ const createNew = async (reqBody) => {
     }
 
     // goi tang model de xu li logic luu vao database
+    const createdBoard = await boardModel.createNew(newBoard)
+    // console.log(createdBoard)
 
+    // lay ban ghi board sau khi goi (tuy muc dich du an ma co can buoc nay hay khong)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
     // lam them cac xu li logic voi cac collection khac tuy dac thu du an,...
     // ban email, notification, log, ... ve cho admin khi co 1 board duoc tao moi
 
     //   trong service luon phai co return de tra ve ket qua cho controller
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
