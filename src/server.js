16aa8c723b50a24f3f53c50e4bf301e21befda82
marchisDev <buiquangchicv0303@gmail.com>
2025-01-20 18:12:57 +0700

    Config CORS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 import express from 'express'
+import cors from 'cors'
+import { corsOptions } from '~/config/cors'
 import AsyncExitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
@@ -9,6 +11,9 @@ import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
+
+  // xu li CORS
+  app.use(cors(corsOptions))
 
   // enable parsing of http request body json data
   app.use(express.json())
