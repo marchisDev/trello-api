4f94bf6afb2235d50123017970b4d5b7155fe001
marchisDev <buiquangchicv0303@gmail.com>
2025-02-05 23:53:24 +0700

    update whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
-  'https://trello-web-taupe.vercel.app/',
-  'https://trello-web-bui-quang-chis-projects.vercel.app/'
+  'https://trello-web-taupe.vercel.app',
+  'https://trello-web-bui-quang-chis-projects.vercel.app'
   // 'http://localhost:5173'
 ]
 
